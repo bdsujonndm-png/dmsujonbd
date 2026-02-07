36932d80cae6dc1a9ff289ff8ef11745e5727c31
bdsujonndm-png <bdsujonndm@gmail.com>
2026-02-07 13:17:14 +0600

    Update copyright notice in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -92,7 +92,7 @@ const Footer: React.FC = () => {
         {/* Final Bottom Bar */}
         <div className="pt-8 border-t border-white/5 flex flex-col md:flex-row justify-between items-center gap-4">
           <p className="text-gray-600 text-[10px] font-black uppercase tracking-[0.4em]">
-            &copy; 2025 Sujon Agency. All Rights Reserved.
+            &copy; 2025 Sujon It Expert. All Rights Reserved.
           </p>
           <div className="flex gap-8 text-[9px] font-black uppercase tracking-[0.2em] text-gray-700">
             <a href="#" className="hover:text-yellow-400 transition-all">Privacy Policy</a>
